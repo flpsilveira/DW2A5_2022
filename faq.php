41c1c69d885f09de73f9157ddeb4f3b3e9a18fb4
flpsilveira <104659793+flpsilveira@users.noreply.github.com>
2022-04-29 21:37:55 -0300

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre</title>
+
+</head>
+
+<body>
+<?php
+        require 'header.php'
+    ?>
+    <br>
+        <h1>FAQ</h1>
+
+            <img class="img-lorem img-thumbnail m-4 rounded float-end" src="https://www.acidadevotuporanga.com.br/Images/Noticia/Grande/00000000191637668878913859183.jpg" alt="Instituto" height = "492" width = "768">
+            <p>
+            FAQ é um acrónimo da expressão inglesa Frequently Asked Questions Em português seria algo como "Perguntas frequentes". Um FAQ, quando usado num contexto pluralista, significa uma compilação de perguntas frequentes acerca de determinado tema; quando usado num contexto singular, será uma dessas perguntas frequentes. 
+
+            </p>
+
+            <br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+<?php
+    require 'footer.php'
+?>
+
+
+</body>
+
+</html>
